05a70f3f63e28370902293943fab81a24c45befb
David Liu <davidsiqiliu@gmail.com>
2020-03-20 00:55:45 -0400

    Removed equals

--- src/main/scala/com/davidsiqiliu/sparklyclean/impl/BKV.scala
+++ src/main/scala/com/davidsiqiliu/sparklyclean/impl/BKV.scala
@@ -2,18 +2,6 @@ package com.davidsiqiliu.sparklyclean.impl
 
 case class BKV(b: Int, v: String) extends Ordered[BKV] {
 
-  def canEqual(a: Any): Boolean = a.isInstanceOf[BKV]
-
-  override def equals(that: Any): Boolean =
-    that match {
-      case that: BKV => {
-        that.canEqual(this) &&
-          this.b == that.b &&
-          this.v == that.v
-      }
-      case _ => false
-    }
-
   override def compare(that: BKV): Int =
     that match {
       case that: BKV => {
